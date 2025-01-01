e63422673915157696d577a3792f19d1f03686d5
William Okech <38021234+wokech@users.noreply.github.com>
2025-01-01 10:46:38 +0300

    new images
    christmas tree and happy new year

---
+++ R_scripts/christmas_tree_1.R
@@ -0,0 +1,8 @@
+# Christmas Trees
+
+#install.packages("christmas")
+
+library(christmas)
+
+xmastreeshape(shape = "piramidal", year = 2024, "english",
+              nballs = 20, ballscolor = c("red", "azure", "gold", "darkgreen"))
